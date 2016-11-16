c6f991ce4a15e84ff6752a6cfb62f873ae18c6b5
Kjell <kjell@leanside.com>
2016-11-16 16:31:10 -0600

    page search results with `?from=`

--- search.js
+++ search.js
@@ -4,7 +4,7 @@ var es = new require('elasticsearch').Client({
   requestTimeout: 3000,
 })
 
-var search = function(query, size, filters, isApp, callback) {
+var search = function(query, size, filters, isApp, from, callback) {
 
 var fields = ["artist.artist^15", "artist.folded^15", "title^11", "description^3", "text^2", "accession_number", "_all", "artist.ngram^2", "title.ngram"]
 if(query.match(/".*"/)) fields = fields.slice(0, -2)
@@ -112,7 +112,17 @@ var function_score_sqs = {
   }
   var highlight = {fields: {"*": {fragment_size: 5000, number_of_fragments: 1}}}
 
-  var search = {index: process.env.ES_index, body: {query: q, aggs: aggs, highlight: highlight, suggest: suggest}, size: size}
+  var search = {
+    index: process.env.ES_index,
+    body: {
+      query: q,
+      aggs: aggs,
+      highlight: highlight,
+      suggest: suggest
+    },
+    size: size,
+    from: from,
+  }
   // when the search is undefined or blank, do a count over the aggregations
   if(query == '' || query == undefined) {
     search = {body: {size: 0, aggs: aggs}, searchType: 'count'}
@@ -142,10 +152,11 @@ app.get('/:query', function(req, res) {
   if(req.params.query == 'favicon.ico') return res.send(404)
   var replies = []
   var size = req.query.size || 100
+  var from = req.query.from || 0
   var filters = req.query.filters
   var userAgent = req.headers['user-agent']
   var isApp = userAgent && userAgent.match('MIA') // 'MIA/8 CFNetwork/758.0.2 Darwin/15.0.0' means this request came frmo the journeys app
-  search(req.params.query || '', size, filters, isApp, function(error, results) {
+  search(req.params.query || '', size, filters, isApp, from, function(error, results) {
     results.query = req.params.query
     results.filters = filters
     results.error = error
