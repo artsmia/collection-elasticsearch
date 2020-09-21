2e5274138ed82f446a07fb333e8a77c4e406ac1d
Kjell <kjell@leanside.com>
2020-09-21 11:13:27 -0500

    move FitD under `ingests`

--- ingests/index.js
+++ ingests/index.js
@@ -8,19 +8,28 @@ const {
 } = require('./artworks/mia')
 
 // readMia({ buckets: [83] })
-readMia()
-  // prettier-ignore
-  .then(output => {
-    // Is it useless to stringify the JSON to a stream just to immediately parse it again?
-    output
-      // .pipe(transform) // TODO learn streams
-      .pipe(ndjson.parse())
-      .on('data', function(obj) {
-        const reshaped = transformMia(obj)
-        if (reshaped.id === '1') console.info({ reshaped })
-      })
-  })
+false &&
+  readMia()
+    // prettier-ignore
+    .then(output => {
+      // Is it useless to stringify the JSON to a stream just to immediately parse it again?
+      output
+        // .pipe(transform) // TODO learn streams
+        .pipe(ndjson.parse())
+        .on('data', function(obj) {
+          const reshaped = transformMia(obj)
+          if (reshaped.id === '1') console.info({ reshaped })
+        })
+    })
 
 // stream()
+
+const {
+  read: readFitD,
+  stream: streamFitD,
+  transform: transformFitD,
+} = require('./artworks/foot-in-the-door')
+
+readFitD()
 
 console.info('ingests/index done.')
